a8f5fee771b5bebe950c374e3870a128b3954b2a
Andres Cusirramos Marquez Mares <andres.cusirramos@ucsp.edu.pe>
2022-10-11 19:27:14 -0500

    parcial

---
+++ Assets/DrawColorFrame.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using NuitrackSDK.Frame;
+
+public class DrawColorFrame : MonoBehaviour
+{
+    [SerializeField] RawImage background;
+
+    void Start()
+    {
+        NuitrackManager.onColorUpdate += DrawColor;
+    }
+    void Destroy()
+    {
+        NuitrackManager.onColorUpdate -= DrawColor;
+    }
+    void DrawColor(nuitrack.ColorFrame frame)
+    {
+        background.texture = frame.ToTexture2D();
+    }
+}
